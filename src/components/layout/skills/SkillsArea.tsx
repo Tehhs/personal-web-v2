b90ff19b2361722087be946bbdd91b044b387ae4
Tehhs <lltsamoa@gmail.com>
2025-05-14 14:16:10 +1000

    getting much closer with infinite scroll on skills

--- src/components/layout/skills/SkillsArea.tsx
+++ src/components/layout/skills/SkillsArea.tsx
@@ -2,15 +2,24 @@
 import { useState, useRef, useEffect } from "react";
 import { SkillItem } from "./SkillItem/SkillItem";
 import {useInView, motion} from 'framer-motion'
+import styled from "styled-components";
 
 export const RandomText = (charNumbers: number) => { 
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;':,.<>?";
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let result = "";
     for (let i = 0; i < charNumbers; i++) {
         result += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     return result;
 }
+
+export const SkillOptions = [
+    {
+        Key: "dfajlnfwe",
+        SkillName:"React",
+        ImageSrc:"/skills/react.svg"
+    }
+]
 
 export const SkillsArea : React.FC = () => { 
     
@@ -19,27 +28,35 @@ export const SkillsArea : React.FC = () => {
     const isFirstElementInView = useInView(firstSkillListingElement, {once: false})
     
     const [skills, setSkills] = useState([
-        {
+        // {
+        //     Key: "dfajlnfwe",
+        //     SkillName:"React",
+        //     ImageSrc:"/skills/react.svg"
+        // }
+    ])
+
+    const [renderInitialSkills, setRenderInitialSkills] = useState(true)
+    const [initialSkills, _] = useState([
+        ...Array(10).fill({
             Key: "dfajlnfwe",
             SkillName:"React",
             ImageSrc:"/skills/react.svg"
-        }
+        })
     ])
 
     useEffect(()=>{
             setInterval(() => {
                 AppendSkill()
             }, 5000)
-        
     }, [])
 
     const AppendSkill = () => { 
         setSkills((prev) => {
-            return [{
+            return [ ...prev, {
                 Key: RandomText(10),
                 SkillName: RandomText(10),
                 ImageSrc: "/skills/react.svg"
-            }, ...prev]
+            }]
         })
     }
     
@@ -49,24 +66,54 @@ export const SkillsArea : React.FC = () => {
         <div>
             this is the skills area 
         </div>
-        <div className="flex gap-5 overflow-hidden">
+        <div className="relative w-full overflow-hidden w-full h-[20rem]">
             {
-                skills.map( (skill, i) => {
-                    return <motion.div
-                        key={skill.Key}
-                        initial={{x: -100}}
-                        animate={{x: 2000}}
-                        transition= {{duration: 100}}
-                        className="absolute"
-                    >
+                [...skills.map( (skill, i) => {
+                    return <MoveContainer key={skill.Key} i={skill.Key} onAnimationEnd={()=>{
+                        setSkills((SKILLS)=>{
+                            return SKILLS.filter( (s) => s != skill)
+                        })
+                    }}>
                         <SkillItem 
                             SkillName={skill.SkillName}
                             ImageSrc={skill.ImageSrc}
                         />
-                    </motion.div>
-                })
+                    </MoveContainer>
+                }),
+                <MoveContainer key={"iniasdtial"} i={"iasdnitial"} className="flex flex-row gap-20" onAnimationEnd={()=>{
+                    setRenderInitialSkills(false) 
+                }}>
+                    {
+                        renderInitialSkills && initialSkills.map(initialSkill => { 
+                            return <SkillItem
+                                SkillName={initialSkill.SkillName}
+                                ImageSrc={initialSkill.ImageSrc}
+                            />
+                        })
+                    }
+                </MoveContainer>
+                ]
             }
         </div>
     </div>
 
 }
+
+export const MoveContainer = styled.div<{i:any}>`
+
+    @keyframes mmm${props => props.i ?? 0} { 
+        from { 
+            margin-left: -20rem; 
+            color: pink;
+        }
+        to { 
+            margin-left: 1500px;
+            color: yellow; 
+        }
+    }
+    position: absolute; 
+
+    animation: mmm${props => props.i ?? 0} 50s linear 1;
+    color: green;
+
+`
